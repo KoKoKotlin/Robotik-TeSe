3ea1271ecb441eb49f3a87c531861e502c9b0374
KoKoKotlin <labruzzler@gmail.com>
2021-05-11 14:29:35 +0200

    added plots for mean and median

--- woche3/gnss_data_paths/plotter.py
+++ woche3/gnss_data_paths/plotter.py
@@ -24,18 +24,70 @@ def load_data(path):
 
     return (lat, lon)
 
-def main():
+def load_mean_data():
+    path = "data/mean_positions.txt"
+
+    mean   = []
+    median = []
+    with open(path, "r") as f:
+        for i, line in enumerate(f):
+            if i == 0: continue
+
+            mean_data, median_data = line.split(":")[1].split(";")
+            mean_   = map(lambda x: float(x.strip()), mean_data.split(","))
+            median_ = map(lambda x: float(x.strip()), median_data.split(","))
+
+            mean.append(mean_)
+            median.append(median_)
+
+    return (mean, median)
+
+def plot_raw():
     paths = [f"data/{i}.txt" for i in range(1, 26)]
 
     for path in paths:
         lat, lon = load_data(path)
-        plt.scatter(lat, lon, marker="o")
+        plt.scatter(lon, lat, marker="o")
   
-    plt.xlabel("latitude")
-    plt.ylabel("longitude")
-    
+    plt.xlabel("longitude")
+    plt.ylabel("latitude")
     
     plt.show()
+
+def plot_mean():
+    mean, median = load_mean_data()
+
+    for point in mean:
+        lat, lon = point
+        plt.scatter(lon, lat, marker="o")
+   
+    plt.xlabel("longitude")
+    plt.ylabel("latitude")
+    
+    plt.show()
+
+def plot_median():
+    mean, median = load_mean_data()
+
+    for point in median:
+        lat, lon = point
+        plt.scatter(lon, lat, marker="o")
+   
+    plt.xlabel("longitude")
+    plt.ylabel("latitude")
+    
+    plt.show()
+
+
+def main():
+    mode = input("[r]aw, [m1]ean, [m2]edian: ")
+
+    if   mode == "r":
+        plot_raw()
+    elif mode == "m1":
+        plot_mean()
+    elif mode == "m2":
+        plot_median()
 
 if __name__ == "__main__":
     main()
